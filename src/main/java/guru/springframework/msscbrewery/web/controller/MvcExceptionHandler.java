703d866d6adddab7e474203581732dad1f853870
BALLNAZZAR-PC\Ballnazzar <ludek.p@email.cz>
2022-08-10 22:13:45 +0200

    Add controller advice

--- src/main/java/guru/springframework/msscbrewery/web/controller/MvcExceptionHandler.java
+++ src/main/java/guru/springframework/msscbrewery/web/controller/MvcExceptionHandler.java
@@ -2,6 +2,7 @@ package guru.springframework.msscbrewery.web.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -13,11 +14,18 @@ import java.util.stream.Collectors;
 public class MvcExceptionHandler {
 
     @ExceptionHandler(ConstraintViolationException.class)
-    public ResponseEntity<List> validationErrorHandler(ConstraintViolationException e) {
+    public ResponseEntity<List> handleValidationException(ConstraintViolationException e) {
         List<String> errors = e.getConstraintViolations().stream()
                 .map(constraintViolation -> constraintViolation.getPropertyPath() + " : " + constraintViolation.getMessage())
                 .collect(Collectors.toList());
+
         return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(BindException.class)
+    public ResponseEntity<List> handleBindException(BindException e) {
+
+        return new ResponseEntity<>(e.getAllErrors(), HttpStatus.BAD_REQUEST);
     }
 
 }
